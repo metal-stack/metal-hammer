e3f9ce916b9067fe18dc023adb6af9531f58a78b
Stefan Majer <stefan.majer@f-i-ts.de>
2018-10-05 09:47:41 +0200

    More hw details in register request

--- cmd/register.go
+++ cmd/register.go
@@ -8,22 +8,83 @@ import (
 	"net/http"
 
 	log "github.com/inconshreveable/log15"
+
+	"github.com/jaypipes/ghw"
 )
 
+type Nic struct {
+	MacAddress string   `json:"mac"`
+	Name       string   `json:"name"`
+	Vendor     string   `json:"vendor"`
+	Features   []string `json:"features"`
+}
+
+type BlockDevice struct {
+	Name string `json:"name"`
+	Size uint64 `json:"size"`
+}
+
 type registerRequest struct {
-	UUID string `json:"uuid" description:"the uuid of the device to register"`
+	UUID     string        `json:"uuid" description:"the uuid of the device to register"`
+	Memory   int64         `json:"memory" description:"the memory in bytes of the device to register"`
+	CPUCores uint32        `json:"cpucores" description:"the cpu core of the device to register"`
+	Nics     []Nic         `json:"nics"`
+	Disks    []BlockDevice `json:"disks"`
 }
 
 // RegisterDevice register a device at the metal-api via metal-core
 func RegisterDevice(spec *Specification) (string, error) {
-	result := registerRequest{}
-	uuid, err := ioutil.ReadFile("/sys/class/dmi/id/product_uuid")
+	hw := registerRequest{}
+
+	memory, err := ghw.Memory()
 	if err != nil {
-		return "", fmt.Errorf("error getting product_uuid info: %v", err)
+		return "", fmt.Errorf("unable to get system memory, info:%v", err)
+	}
+	hw.Memory = memory.TotalPhysicalBytes
+
+	cpu, err := ghw.CPU()
+	if err != nil {
+		return "", fmt.Errorf("unable to get system cpu(s), info:%v", err)
+	}
+	hw.CPUCores = cpu.TotalCores
+
+	net, err := ghw.Network()
+	if err != nil {
+		return "", fmt.Errorf("unable to get system nic(s), info:%v", err)
+	}
+	nics := []Nic{}
+	for _, n := range net.NICs {
+		nic := Nic{
+			MacAddress: n.MacAddress,
+			Name:       n.Name,
+			Vendor:     n.Vendor,
+			Features:   n.EnabledFeatures,
+		}
+		nics = append(nics, nic)
+	}
+	hw.Nics = nics
+
+	blockInfo, err := ghw.Block()
+	if err != nil {
+		return "", fmt.Errorf("unable to get system block devices, info:%v", err)
+	}
+	disks := []BlockDevice{}
+	for _, disk := range blockInfo.Disks {
+		blockDevice := BlockDevice{
+			Name: disk.Name,
+			Size: disk.SizeBytes,
+		}
+		disks = append(disks, blockDevice)
+	}
+	hw.Disks = disks
+
+	boardSerial, err := ioutil.ReadFile("/sys/class/dmi/id/board_serial")
+	if err != nil {
+		return "", fmt.Errorf("error getting board_serial info: %v", err)
 	}
 
-	result.UUID = string(uuid)
-	return register(spec.ReportURL, result)
+	hw.UUID = string(boardSerial)
+	return register(spec.ReportURL, hw)
 }
 
 func register(url string, hw registerRequest) (string, error) {
