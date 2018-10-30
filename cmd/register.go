82b47077b8a3e2c458cf1948fa19506b1ef0010c
Stefan Majer <stefan.majer@f-i-ts.de>
2018-10-30 11:52:19 +0100

    More logging for register

--- cmd/register.go
+++ cmd/register.go
@@ -192,10 +192,12 @@ func register(url string, hw registerRequest) (string, error) {
 		return "", fmt.Errorf("POST of hw %s to register endpoint:%s did not succeed %v response body:%s", string(hwJSON), url, resp.Status, body)
 	}
 
+	log.Info("register device returned", "response", body)
 	result := make(map[string]string)
 	var uuid string
 	err = json.Unmarshal(body, &result)
 	if err != nil {
+		log.Error("unable to unmarshal register response", "error", err)
 		uuid = "unknown"
 	} else {
 		uuid = result["id"]
