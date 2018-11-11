9763bdc9c80198a35c2223853c145a5ba21148a7
Stefan Majer <stefan.majer@f-i-ts.de>
2018-11-11 15:13:02 +0100

    create a ipmi user with configurable privileges

---
+++ pkg/ipmi/ipmi.go
@@ -0,0 +1,114 @@
+package ipmi
+
+// IPMI Wiki
+// https://www.thomas-krenn.com/de/wiki/IPMI_Konfiguration_unter_Linux_mittels_ipmitool
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"reflect"
+	"strings"
+)
+
+// Privilege of a ipmitool user
+type Privilege int
+
+const (
+	Callback      = Privilege(1)
+	User          = Privilege(2)
+	Operator      = Privilege(3)
+	Administrator = Privilege(4)
+	OEM           = Privilege(5)
+	NoAccess      = Privilege(15)
+)
+
+// LanConfig contains the config of ipmi.
+// tag must contain first column name of ipmitool lan print command output
+// to get the second column value be parsed into the field.
+type LanConfig struct {
+	IP  string `ipmitool:"IP Address"`
+	Mac string `ipmitool:"MAC Address"`
+}
+
+// GetLanConfig returns the LanConfig
+func GetLanConfig() (LanConfig, error) {
+	config := LanConfig{}
+
+	cmdOutput, err := ipmitool("lan", "print")
+	if err != nil {
+		return config, fmt.Errorf("unable to execute ipmitool info:%v", err)
+	}
+	lanConfigMap := getLanConfig(cmdOutput)
+
+	config.from(lanConfigMap)
+
+	return config, nil
+}
+
+// CreateUser create a ipmi user with password and privilege level
+func CreateUser(username, password string, uid int, privilege Privilege) error {
+	_, err := ipmitool("user", "set", "name", string(uid), username)
+	if err != nil {
+		return fmt.Errorf("unable to create user %s info: %v", username, err)
+	}
+	_, err = ipmitool("user", "set", "password", string(uid), password)
+	if err != nil {
+		return fmt.Errorf("unable to set password for user %s info: %v", username, err)
+	}
+	channelnumber := "1"
+	_, err = ipmitool("channel", "setaccess", channelnumber, string(uid), "link=on", "ipmi=on", "callin=on", fmt.Sprintf("privilege=%d", int(privilege)))
+	if err != nil {
+		return fmt.Errorf("unable to set privilege for user %s info: %v", username, err)
+	}
+	_, err = ipmitool("user", "enable", string(uid))
+	if err != nil {
+		return fmt.Errorf("unable to enable user %s info: %v", username, err)
+	}
+	return nil
+}
+
+func getLanConfig(cmdOutput string) map[string]string {
+	result := make(map[string]string)
+	scanner := bufio.NewScanner(strings.NewReader(cmdOutput))
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) < 2 {
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(strings.Join(parts[1:], ""))
+		result[key] = value
+	}
+	return result
+}
+
+var ipmitoolCommand = "ipmitool"
+
+func ipmitool(arg ...string) (string, error) {
+	path, err := exec.LookPath(ipmitoolCommand)
+	if err != nil {
+		return "", fmt.Errorf("unable to locate program:%s in path info:%v", ipmitoolCommand, err)
+	}
+	cmd := exec.Command(path, arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	output, err := cmd.Output()
+
+	return string(output), err
+}
+
+// from uses reflection to fill the LanConfig struct based on the tags on it.
+func (c *LanConfig) from(output map[string]string) {
+	val := reflect.ValueOf(c).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		valueField := val.Field(i)
+		typeField := val.Type().Field(i)
+		tag := typeField.Tag
+
+		ipmitoolKey := tag.Get("ipmitool")
+		valueField.SetString(output[ipmitoolKey])
+	}
+}
---
+++ pkg/ipmi/ipmi_test.go
@@ -0,0 +1,123 @@
+package ipmi
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Output of root@ipmitest:~# ipmitool lan print
+const lanPrint = `
+Set in Progress         : Set Complete
+Auth Type Support       : NONE MD2 MD5 PASSWORD
+Auth Type Enable        : Callback : MD2 MD5 PASSWORD
+                        : User     : MD2 MD5 PASSWORD
+                        : Operator : MD2 MD5 PASSWORD
+                        : Admin    : MD2 MD5 PASSWORD
+                        : OEM      : MD2 MD5 PASSWORD
+IP Address Source       : Static Address
+IP Address              : 10.248.36.246
+Subnet Mask             : 255.255.252.0
+MAC Address             : 0c:c4:7a:ed:3e:27
+SNMP Community String   : public
+IP Header               : TTL=0x00 Flags=0x00 Precedence=0x00 TOS=0x00
+BMC ARP Control         : ARP Responses Enabled, Gratuitous ARP Disabled
+Default Gateway IP      : 10.248.36.1
+Default Gateway MAC     : 30:b6:4f:c3:a0:c1
+Backup Gateway IP       : 0.0.0.0
+Backup Gateway MAC      : 00:00:00:00:00:00
+802.1q VLAN ID          : Disabled
+802.1q VLAN Priority    : 0
+RMCP+ Cipher Suites     : 1,2,3,6,7,8,11,12
+Cipher Suite Priv Max   : XaaaXXaaaXXaaXX
+                        :     X=Cipher Suite Unused
+                        :     c=CALLBACK
+                        :     u=USER
+                        :     o=OPERATOR
+                        :     a=ADMIN
+                        :     O=OEM
+Bad Password Threshold  : Not Available
+`
+
+func Test_getLanConfig(t *testing.T) {
+	tests := []struct {
+		name      string
+		cmdOutput string
+		want      map[string]string
+	}{
+		{
+			name:      "simple",
+			cmdOutput: lanPrint,
+			want: map[string]string{
+				"IP Address":  "10.248.36.246",
+				"Subnet Mask": "255.255.252.0",
+				"MAC Address": "0c:c4:7a:ed:3e:27",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getLanConfig(tt.cmdOutput)
+			for key, value := range tt.want {
+				if got[key] != value {
+					t.Errorf("getLanConfig() = %v, want %v", got[key], value)
+				}
+			}
+		})
+	}
+}
+
+func TestGetLanConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    LanConfig
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetLanConfig()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetLanConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetLanConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLanConfig_From(t *testing.T) {
+	type fields struct {
+		IP  string
+		Mac string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		output map[string]string
+	}{
+		{
+			name: "simple",
+			fields: fields{
+				IP:  "10.248.36.246",
+				Mac: "0c:c4:7a:ed:3e:27",
+			},
+			output: map[string]string{
+				"IP Address":  "10.248.36.246",
+				"Subnet Mask": "255.255.252.0",
+				"MAC Address": "0c:c4:7a:ed:3e:27",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &LanConfig{
+				IP:  tt.fields.IP,
+				Mac: tt.fields.Mac,
+			}
+			c.from(tt.output)
+		})
+	}
+}
