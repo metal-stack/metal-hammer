7b8905ee3f6798bb02f0a8c8e1e8271d6813d772
Stefan Majer <stefan.majer@f-i-ts.de>
2018-11-12 11:20:37 +0100

    Adopt md5sum detection for kernel download

--- download-kernel.sh
+++ download-kernel.sh
@@ -1,5 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Intentionally added to download metal-hammer-kernel.
+set -e
 
-if [ ! -f metal-hammer-kernel ]; then
-  curl -fSO https://blobstore.fi-ts.io/metal/images/metal-hammer-kernel
-fi
+BLOBSTORE=https://blobstore.fi-ts.io
+
+dirty() {
+    curl \
+        --fail \
+        --location \
+        --remote-name \
+        --silent \
+        "${BLOBSTORE}/metal/images/${1}.md5"
+    local res=$(md5sum --check "${1}.md5" 2>/dev/null 1>&2; echo $?)
+    echo "${res}"
+}
+
+download() {
+    curl \
+        --fail \
+        --location \
+        --remote-name \
+        "${BLOBSTORE}/metal/images/$1"
+}
+
+download_if_dirty() {
+    local isDirty=$(dirty "${1}")
+    if [[ "$isDirty" = "1" ]]
+    then
+        echo "Downloading $i..."
+        download ${1}
+    fi
+}
+
+for i in "metal-hammer-kernel"
+do
+    download_if_dirty $i
+done
+
+# Ensure files remain writable for group to enable re-download (libvirt takes ownership for unkown reson)
+chmod 660 metal-hammer-* 2>/dev/null || true
