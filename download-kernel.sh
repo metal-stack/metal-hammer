d9149dda90f61dda164e809ef67d37de339ae09e
Stefan Majer <stefan.majer@f-i-ts.de>
2018-11-28 12:39:23 +0100

    Revert download initrd together with kernel, because otherwise the local build will always be overridden

--- download-kernel.sh
+++ download-kernel.sh
@@ -11,8 +11,8 @@ dirty() {
         --location \
         --remote-name \
         --silent \
-        "${BLOBSTORE}/metal/images/metal-hammer/${1}.md5"
-    local res=$(md5sum --check $(basename "${1}") 2>/dev/null 1>&2; echo $?)
+        "${BLOBSTORE}/metal/images/metal-hammer/dev/${1}.md5"
+    local res=$(md5sum --check "${1}.md5" 2>/dev/null 1>&2; echo $?)
     echo "${res}"
 }
 
@@ -21,19 +21,19 @@ download() {
         --fail \
         --location \
         --remote-name \
-        "${BLOBSTORE}/metal/images/metal-hammer/${1}"
+        "${BLOBSTORE}/metal/images/metal-hammer/dev/$1"
 }
 
 download_if_dirty() {
     local isDirty=$(dirty "${1}")
     if [[ "$isDirty" = "1" ]]
     then
-        echo "Downloading ${1}..."
+        echo "Downloading $i..."
         download ${1}
     fi
 }
 
-for i in "dev/metal-hammer-kernel" "metal-hammer-initrd.img.lz4"
+for i in "metal-hammer-kernel"
 do
     download_if_dirty $i
 done
