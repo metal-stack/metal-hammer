3cd7167f4ac17b39d11be672be483bfbf61f5a13
Sandro Koll <sandro.koll@f-i-ts.de>
2020-11-04 13:58:23 +0100

    improve readability

--- cmd/supwd.go
+++ cmd/supwd.go
@@ -16,42 +16,26 @@ func (c *GrpcClient) newSuperUserPasswordClient() (v1.SuperUserPasswordClient, i
 	return v1.NewSuperUserPasswordClient(conn), conn, nil
 }
 
-// fetchSuperUserPassword tries to fetch the bmc superuser password from metla-api.
-// If no superuser password has been set in metal-api it returns an empty string and true as
-// the second return value, which indicates to skip further processing regarding the superuser password.
-// Otherwise that second return value is always false.
-func (c *GrpcClient) fetchSuperUserPassword() (string, bool, error) {
-	client, closer, err := c.newSuperUserPasswordClient()
+// createBmcSuperuser creates the bmc super user.
+func (h *Hammer) createBmcSuperuser() error {
+	client, closer, err := h.GrpcClient.newSuperUserPasswordClient()
 	if err != nil {
-		return "", false, err
+		return err
 	}
 	defer closer.Close()
 
 	req := &v1.SuperUserPasswordRequest{}
 	resp, err := client.FetchSuperUserPassword(context.Background(), req)
 	if err != nil {
-		return "", false, err
-	}
-
-	if resp.GetFeatureDisabled() {
-		return "", true, nil
-	}
-
-	return resp.GetSuperUserPassword(), false, nil
-}
-
-func (h *Hammer) createBmcSuperuser() error {
-	pwd, featureDisabled, err := h.GrpcClient.fetchSuperUserPassword()
-	if err != nil {
 		return errors.Wrap(err, "failed to fetch SuperUser password")
 	}
 
-	if featureDisabled {
+	if resp.FeatureDisabled {
 		return nil
 	}
 
 	bmcConn := h.Hal.BMCConnection()
-	err = bmcConn.CreateUser(bmcConn.SuperUser(), api.AdministratorPrivilege, pwd)
+	err = bmcConn.CreateUser(bmcConn.SuperUser(), api.AdministratorPrivilege, resp.SuperUserPassword)
 	if err != nil {
 		return errors.Wrapf(err, "failed to create bmc superuser: %s", bmcConn.SuperUser().Name)
 	}
