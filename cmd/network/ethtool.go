065c622892dd103da85803fd5b6ab54b230881c6
Stefan Majer <stefan.majer@f-i-ts.de>
2021-03-18 13:57:52 +0100

    fix log entry

--- cmd/network/ethtool.go
+++ cmd/network/ethtool.go
@@ -98,7 +98,7 @@ var buggyIntelNicDriverNames = []string{"i40e", "ice"}
 // or a loop over all directories in /sys/kernel/debug/i40e/*/command
 func (e *Ethtool) stopFirmwareLLDP() {
 	for _, driver := range buggyIntelNicDriverNames {
-		log.Info("ethtool", "stopFirmwareLLDP", "driver", driver)
+		log.Info("ethtool", "stopFirmwareLLDP for driver", driver)
 		debugFSPath := path.Join("/sys/kernel/debug", driver)
 		err := filepath.Walk(debugFSPath, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
