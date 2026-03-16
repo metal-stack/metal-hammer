47efb828868144dd56bb0fdbbd72d84ba2dd52ff
Stefan Majer <stefan.majer@f-i-ts.de>
2026-03-16 12:22:20 +0100

    Dump stack

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"runtime/debug"
 	"syscall"
 	"time"
 
@@ -94,7 +95,8 @@ func main() {
 	// FIXME set loglevel from spec.Debug
 	defer func() {
 		if r := recover(); r != nil {
-			log.Error("recover, sleeping for 5sec.", "errortrace", r)
+			stack := debug.Stack()
+			log.Error("recover, sleeping for 5sec.", "errortrace", r, "stack", string(stack))
 			time.Sleep(5 * time.Second)
 		}
 	}()
