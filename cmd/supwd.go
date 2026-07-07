c98aa5599aa538ad39cfd39e9f468ff1276d1fdc
Simon Mayer <Simon.Mayer@x-cellent.com>
2026-07-07 11:44:30 +0200

    Test

--- cmd/supwd.go
+++ cmd/supwd.go
@@ -23,28 +23,14 @@ func (h *hammer) createBmcSuperuser() error {
 
 	bmcConn := h.hal.BMCConnection()
 
-	changeIsNeeded, err := bmcConn.NeedsPasswordChange(bmcConn.SuperUser(), resp.SuperUserPassword)
+	err = bmcConn.CreateUser(bmcConn.SuperUser(), api.AdministratorPrivilege, resp.SuperUserPassword)
 	if err != nil {
-		if !changeIsNeeded {
-			// metal-hammer should fail
-			// it is not the case, that the user is missing or the password is already correct for the superuser
-			h.log.Error("failed to verify password change for bmc superuser", "user", bmcConn.SuperUser().Name, "error", err)
-		}
-
-		// Proceed: Password need to be updated or superuser created initially
-		if err := bmcConn.CreateUser(bmcConn.SuperUser(), api.AdministratorPrivilege, resp.SuperUserPassword); err != nil {
-			stillNeeded, e := bmcConn.NeedsPasswordChange(bmcConn.SuperUser(), resp.SuperUserPassword)
-			if e != nil || stillNeeded {
-				return fmt.Errorf("failed to configure bmc superuser %s: %w", bmcConn.SuperUser().Name, e)
-			}
-			h.log.Warn("bmc reported an error configuring superuser, but password verification succeeded, proceed",
-				"user", bmcConn.SuperUser().Name, "error", err)
-		} else {
-			h.log.Info("created superuser", "user", bmcConn.SuperUser().Name)
-		}
+		// FIXME: this happens always after the first creation on X12 and newer boards
+		// return fmt.Errorf("failed to create bmc superuser: %s %w", bmcConn.SuperUser().Name, err)
+		h.log.Error("failed to create bmc superuser", "user", bmcConn.SuperUser().Name, "error", err)
 		return nil
 	}
 
-	h.log.Info("superuser already present and password correct", "user", bmcConn.SuperUser().Name)
+	h.log.Info("created superuser", "user", bmcConn.SuperUser().Name)
 	return nil
 }
